eda690195b8c3cb546329a3075afc72588997810
miloserdov <smiloserdov@citrobyte.com>
2022-07-03 13:37:09 +0600

    next

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import '@/assets/styles/base.css'
+// import '@/assets/styles/base.css'
 import 'devextreme/dist/css/dx.common.css'
 import 'devextreme/dist/css/dx.light.css'
 import '@/assets/styles/dx-styles.scss'
