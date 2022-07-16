e396ce34c376189b2639d778991b9ac7dea204f1
miloserdov <smiloserdov@citrobyte.com>
2022-07-16 13:21:30 +0600

    next

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
 // import '@/assets/styles/base.css'
-import 'devextreme/dist/css/dx.common.css'
-import 'devextreme/dist/css/dx.light.css'
-import '@/assets/styles/dx-styles.scss'
-import '@/assets/styles/global.scss'
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import '@/assets/styles/dx-styles.scss';
+import '@/assets/styles/global.scss';
 
-import ruMessages from 'devextreme/localization/messages/ru.json'
-import { locale, loadMessages } from 'devextreme/localization'
-loadMessages(ruMessages)
-locale(navigator.language)
+import ruMessages from 'devextreme/localization/messages/ru.json';
+import { locale, loadMessages } from 'devextreme/localization';
+loadMessages(ruMessages);
+locale(navigator.language);
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
 
 // console.log(import.meta.env)
--- src/templates/bigdata/bigdata-dx-load.vue
+++ src/templates/bigdata/bigdata-dx-load.vue
@@ -178,168 +178,168 @@
 </template>
 
 <script setup>
-import Axios from '../../axios/reqAxios';
-import { ref, reactive } from 'vue';
-import DxTextBox from 'devextreme-vue/text-box';
-import DxDateBox from 'devextreme-vue/date-box';
-import { DxButton } from 'devextreme-vue/button';
-import notify from 'devextreme/ui/notify';
-import {
-    DxDataGrid,
-    DxColumn,
-    DxPaging,
-    DxPager,
-    DxSelection,
-    DxExport,
-    DxColumnChooser,
-    DxSorting,
-    DxToolbar,
-    DxItem,
-    DxMasterDetail
-} from 'devextreme-vue/data-grid';
-import DetailsTemplate from './details-template.vue';
-import CustomStore from 'devextreme/data/custom_store';
+    import Axios from '../../axios/reqAxios';
+    import { ref, reactive } from 'vue';
+    import DxTextBox from 'devextreme-vue/text-box';
+    import DxDateBox from 'devextreme-vue/date-box';
+    import { DxButton } from 'devextreme-vue/button';
+    import notify from 'devextreme/ui/notify';
+    import {
+        DxDataGrid,
+        DxColumn,
+        DxPaging,
+        DxPager,
+        DxSelection,
+        DxExport,
+        DxColumnChooser,
+        DxSorting,
+        DxToolbar,
+        DxItem,
+        DxMasterDetail
+    } from 'devextreme-vue/data-grid';
+    import DetailsTemplate from './details-template.vue';
+    import CustomStore from 'devextreme/data/custom_store';
 
-const loading = ref(false);
+    const loading = ref(false);
 
-// let pageData = ref({
-//     pageSize: 10,
-//     pageIndex: 0,
-//     sort: 'id,desc'
-// });
+    // let pageData = ref({
+    //     pageSize: 10,
+    //     pageIndex: 0,
+    //     sort: 'id,desc'
+    // });
 
-let pageSize = ref(10);
-let pageIndex = ref(0);
-let sort = ref('id,desc');
+    let pageSize = ref(10);
+    let pageIndex = ref(0);
+    let sort = ref('id,desc');
 
-// ----------- filter inputs ---------------------------------
+    // ----------- filter inputs ---------------------------------
 
-const filterForm = reactive({
-    cardInput: null,
-    phoneInput: null,
-    dateInput: null
-});
+    const filterForm = reactive({
+        cardInput: null,
+        phoneInput: null,
+        dateInput: null
+    });
 
-// ------------ table ------------------------------
+    // ------------ table ------------------------------
 
-const dataGridKey = ref(0);
-const forceRerender = () => (dataGridKey.value += 1);
-const dataSource = ref(null);
-const store = new CustomStore({
-    key: 'id',
-    async load() {
-        let params = {
-            page: pageIndex.value + 1,
-            size: pageSize.value,
-            sort: sort.value,
-            date: new Date()
-        };
-        try {
-            const { data } = await Axios.get(
-                'manager-api/v2/campaign/campaigns/page',
-                {
-                    params: params
-                }
-            );
-            const info = {
-                data: data.content,
-                totalCount: data.totalElements
-                // summary: r.data.summary,
-                // groupCount: r.data.groupCount
+    const dataGridKey = ref(0);
+    const forceRerender = () => (dataGridKey.value += 1);
+    const dataSource = ref(null);
+    const store = new CustomStore({
+        key: 'id',
+        async load() {
+            let params = {
+                page: pageIndex.value + 1,
+                size: pageSize.value,
+                sort: sort.value,
+                date: new Date()
             };
-            notify(
-                {
-                    message: `The button was clicked`,
-                    width: 300,
-                    // height: 100,
-                    // shading: false,
-                    closeOnOutsideClick: true,
-                    hideOnParentScroll: true,
-                    // position: 'center center'
-                    position: {
-                        at: 'top right',
-                        my: 'top right',
-                        offset: '-50 50'
+            try {
+                const { data } = await Axios.get(
+                    'manager-api/v2/campaign/campaigns/page',
+                    {
+                        params: params
                     }
-                },
-                'success',
-                121500
-            );
-            return info;
-        } catch (error) {
-            console.log(error);
+                );
+                const info = {
+                    data: data.content,
+                    totalCount: data.totalElements
+                    // summary: r.data.summary,
+                    // groupCount: r.data.groupCount
+                };
+                notify(
+                    {
+                        message: `The button was clicked`,
+                        width: 300,
+                        // height: 100,
+                        // shading: false,
+                        closeOnOutsideClick: true,
+                        hideOnParentScroll: true,
+                        // position: 'center center'
+                        position: {
+                            at: 'top right',
+                            my: 'top right',
+                            offset: '-50 50'
+                        }
+                    },
+                    'success',
+                    121500
+                );
+                return info;
+            } catch (error) {
+                console.log(error);
+            }
         }
-    }
-});
+    });
 
-dataSource.value = store;
+    dataSource.value = store;
 
-// ----------  Selects -----------------------
+    // ----------  Selects -----------------------
 
-const selectedRows = ref([]);
+    const selectedRows = ref([]);
 
-const onSelectionChanged = (e) => {
-    selectedRows.value = e.selectedRowKeys;
-    console.log('onSelectionChanged', e.selectedRowKeys);
-    console.log(selectedRows.value);
-};
+    const onSelectionChanged = (e) => {
+        selectedRows.value = e.selectedRowKeys;
+        console.log('onSelectionChanged', e.selectedRowKeys);
+        console.log(selectedRows.value);
+    };
 
-// ---------- Events ----------------------
+    // ---------- Events ----------------------
 
-const logEvent = (e) => console.log(employees);
+    const logEvent = (e) => console.log(employees);
 
-// --------------------------------
+    // --------------------------------
 
-const handlePropertyChange = (e) => {
-    if (e.fullName === 'paging.pageSize') {
-        forceRerender();
-    }
-    // console.log('handlePropertyChange', e);
+    const handlePropertyChange = (e) => {
+        if (e.fullName === 'paging.pageSize') {
+            forceRerender();
+        }
+        // console.log('handlePropertyChange', e);
 
-    handleSort(e);
-};
+        handleSort(e);
+    };
 
-const handleSort = (e) => {
-    if (e.fullName === 'columns[0].sortOrder') {
-        sort.value = e.value === 'asc' ? 'id, desc' : 'id, asc';
-    }
+    const handleSort = (e) => {
+        if (e.fullName === 'columns[0].sortOrder') {
+            sort.value = e.value === 'asc' ? 'id, desc' : 'id, asc';
+        }
 
-    if (e.fullName === 'columns[1].sortOrder') {
-        sort.value =
-            e.value === 'asc' ? 'conditions_ml, desc' : 'conditions_ml, asc';
-    }
+        if (e.fullName === 'columns[1].sortOrder') {
+            sort.value =
+                e.value === 'asc' ? 'conditions_ml, desc' : 'conditions_ml, asc';
+        }
 
-    if (e.fullName === 'columns[2].sortOrder') {
-        sort.value =
-            e.value === 'asc' ? 'description_ml, desc' : 'description_ml, asc';
-    }
+        if (e.fullName === 'columns[2].sortOrder') {
+            sort.value =
+                e.value === 'asc' ? 'description_ml, desc' : 'description_ml, asc';
+        }
 
-    if (e.fullName === 'columns[3].sortOrder') {
-        sort.value =
-            e.value === 'asc'
-                ? 'short_conditions_ml, desc'
-                : 'short_conditions_ml, asc';
-    }
+        if (e.fullName === 'columns[3].sortOrder') {
+            sort.value =
+                e.value === 'asc'
+                    ? 'short_conditions_ml, desc'
+                    : 'short_conditions_ml, asc';
+        }
 
-    if (e.fullName === 'columns[4].sortOrder') {
-        sort.value =
-            e.value === 'asc'
-                ? 'short_description_ml, desc'
-                : 'short_description_ml, asc';
-    }
+        if (e.fullName === 'columns[4].sortOrder') {
+            sort.value =
+                e.value === 'asc'
+                    ? 'short_description_ml, desc'
+                    : 'short_description_ml, asc';
+        }
 
-    if (e.fullName === 'columns[5].sortOrder') {
-        sort.value = e.value === 'asc' ? 'title_ml, desc' : 'title_ml, asc';
-    }
+        if (e.fullName === 'columns[5].sortOrder') {
+            sort.value = e.value === 'asc' ? 'title_ml, desc' : 'title_ml, asc';
+        }
 
-    if (e.fullName === 'columns[6].sortOrder') {
-        sort.value = e.value === 'asc' ? 'dt_created, desc' : 'dt_created, asc';
-    }
+        if (e.fullName === 'columns[6].sortOrder') {
+            sort.value = e.value === 'asc' ? 'dt_created, desc' : 'dt_created, asc';
+        }
 
-    if (e.fullName === 'columns[6].sortOrder') {
-        sort.value = e.value === 'asc' ? 'dt_end, desc' : 'dt_end, asc';
-    }
-};
+        if (e.fullName === 'columns[6].sortOrder') {
+            sort.value = e.value === 'asc' ? 'dt_end, desc' : 'dt_end, asc';
+        }
+    };
 
-// --------------------------------
+    // --------------------------------
 </script>
